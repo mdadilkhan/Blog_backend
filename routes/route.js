1938cb4520e386e19a828cc5e221d643e7962fab
Md Adil Khan <mdadilakhtar8@gmail.com>
2023-03-26 22:35:36 +0530

    bug in deleting comment

--- routes/route.js
+++ routes/route.js
@@ -9,6 +9,7 @@ import { createPost,
          deletePost }  from '../controller/post-controller.js';
 import { authenticateToken } from '../controller/jwt-controller.js';
 import upload from '../utils/upload.js';
+import { getComments, newComment, deleteComment} from '../controller/comment-controller.js';
 
 
 
@@ -28,5 +29,8 @@ router.get('/posts',authenticateToken,getAllPosts);
 router.get('/post/:id',authenticateToken,getPost);
 router.put('/update/:id',authenticateToken,updatePost)
 router.delete('/delete/:id',authenticateToken,deletePost);
+router.post('/comment/new',authenticateToken,newComment);
+router.get('/comments/:id',authenticateToken,getComments);
+router.delete('comment/delete/:id',authenticateToken,deleteComment);
 
 export default router;   
